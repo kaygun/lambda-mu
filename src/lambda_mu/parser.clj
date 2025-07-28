374719a5bfe26ad8cc174aa0b5d298498e7953de
kaygun <atabey_kaygun@hotmail.com>
2025-07-28 15:59:54 +0300

    Major cleanup.

--- src/lambda_mu/parser.clj
+++ src/lambda_mu/parser.clj
@@ -18,26 +18,6 @@
      "
     :auto-whitespace :standard))
 
-(ns lambda_mu.parser
-  (:require [instaparse.core :as insta]
-            [lambda_mu.types :refer [->Var ->Lam ->Appl ->Cont ->Mu]]
-            [clojure.test :refer :all]))
-
-;; === Lambda-Mu Parser using Instaparse ===
-(def parser
-  (insta/parser 
-     "expr = appl
-     appl = term (term)*
-     term = cont | atom
-     atom = lam | mu | var | <'('> expr <')'>
-     lam = <('\\\\' | 'λ')> var <'.'> expr
-     mu  = <('#' | 'μ')> var <'.'> expr
-     cont = <'['> var <']'> atom
-     var = #'[a-zA-Z_][a-zA-Z0-9_]*'
-     ws = #'\\s+'
-     "
-    :auto-whitespace :standard))
-
 (defn to-ast [tree]
   (insta/transform
    {:var  ->Var
